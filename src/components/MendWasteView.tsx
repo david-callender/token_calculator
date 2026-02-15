695fa4cb9dc7eacbdbb2ec59bbe5fe70c2859386
Wade7wastaken <86390605+Wade7wastaken@users.noreply.github.com>
2026-02-15 02:17:34 -0600

    proper token calculations

--- src/components/MendWasteView.tsx
+++ src/components/MendWasteView.tsx
@@ -48,6 +48,7 @@ export const MendWasteView: FC<Props> = ({ state }) => {
 
     return <>
         <div>
+            <div>Tokens: {state.tokens}</div>
             <WasteView entries={WasteEntries} state={state}/>
             <MendView entries={MendEntries}  state={state}/>
         </div>
